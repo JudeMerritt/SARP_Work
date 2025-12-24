55db448b6fd8d6ac48a7c499c9c9cb1a44513d6c
Jude Merritt <jm@Judes-MacBook-Air-502.local>
2025-12-24 14:53:54 -0800

    wrote delay util driver

---
+++ src/util/delay.c
@@ -0,0 +1,29 @@
+#pragma once 
+#include <stdint.h>
+#include "internal/mmio.h"
+
+// ((100 * 1,000,000) * 0.001 - 1)
+#define RELOAD_VAL 0x1869F
+
+void delay(uint32_t delay) {
+    if (delay == 0) return;
+
+    //Program reload value
+    WRITE_FIELD(STK_RVR, STK_RVR_RELOAD, 0x1869F);
+
+    //Clear current value
+    WRITE_FIELD(STK_CVR, STK_CVR_CURRENT, 0);
+
+    //Set clock source 
+    WRITE_FIELD(STK_CSR, STK_CSR_CLKSOURCE, 1);
+
+    //Enable SysTick
+    WRITE_FIELD(STK_CSR, STK_CSR_ENABLE, 1);
+
+    //Run delay loop
+    for (int i = 0; i < delay; i++) {   
+        while (READ_FIELD(STK_CSR, STK_CSR_COUNTFLAG) == 0){}
+    }
+
+    WRITE_FIELD(STK_CSR, STK_CSR_ENABLE, 0);
+}
--- src/util/delay.h
+++ src/util/delay.h
@@ -1,1 +1,4 @@
+#pragma once
 #include <stdint.h>
+
+void delay(uint32_t delay);
