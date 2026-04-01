1c2d4bae54e5e323a19501c75c389e038d85c0c1
Jude Merritt <jm@Judes-MacBook-Air-744.local>
2026-03-31 23:24:23 -0700

    ADC driver V1 complete

--- myWork/adc.c
+++ myWork/adc.c
@@ -1,4 +1,23 @@
-// Written by Jude Merritt
+/**
+ * This file is part of the Titan Flight Computer Project
+ * Copyright (c) 2026 UW SARP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file devices/adc.h
+ * @authors Jude Merritt
+ * @brief ADS124S0x ADC driver
+ */
 
 #include <stdint.h>
 #include "myWork/adc.h"
@@ -12,7 +31,16 @@
 #define STOP  0x0A
 #define RDATA 0x12
 #define STATUS_REG 0x01
-// The largest transfer that will be done with the spi_rreg function
+#define INPMUX_REG 0x02
+#define PGA_REG 0x03
+#define REF_REG 0x05
+#define IDACMAG_REG 0x06
+#define IDACMUX_REG 0x07
+#define GPIODAT_REG 0x10
+#define GPIOCON_REG 0x11
+#define RDY_FLAG 0x40
+#define READ_BIT 0x20
+#define WRITE_BIT 0x40
 #define MAX_RREG_SIZE 6
 
 struct adc_spi_dev DEV;
@@ -26,7 +54,7 @@ static int spi_rreg(uint8_t reg_addr, uint8_t data_size, ti_errc_t* errc) {
     uint8_t src[MAX_RREG_SIZE] = {0};
     uint8_t dst[MAX_RREG_SIZE] = {0};
 
-    src[0] = 0x20 | reg_addr;
+    src[0] = READ_BIT | reg_addr;
     src[1] = data_size - 1;
 
     // Two command bytes + the number of registers to read
@@ -45,16 +73,16 @@ static int spi_rreg(uint8_t reg_addr, uint8_t data_size, ti_errc_t* errc) {
     return result;
 }
 
-static int spi_wreg(uint8_t reg_addr, uint16_t data_size, uint32_t data, ti_errc_t* errc) {
+static void spi_wreg(uint8_t reg_addr, uint16_t data_size, uint32_t data, ti_errc_t* errc) { 
     if (data_size > (MAX_RREG_SIZE - 2) || data_size == 0) {
         *errc = TI_ERRC_INVALID_ARG;
-        return -1;
+        return;
     }
 
     uint8_t src[MAX_RREG_SIZE] = {0};
     uint8_t dst[MAX_RREG_SIZE] = {0};
 
-    src[0] = 0x40 | reg_addr;
+    src[0] = WRITE_BIT | reg_addr;
     src[1] = data_size - 1;
 
     for (int i = 0; i < data_size; i++) {
@@ -64,23 +92,23 @@ static int spi_wreg(uint8_t reg_addr, uint16_t data_size, uint32_t data, ti_errc
 
     uint8_t tot_size = 2 + data_size;
     *errc = spi_transfer_sync(DEV.inst, DEV.ss_pin, src, dst, tot_size);
-
-    if (*errc != TI_ERRC_NONE) {
-        return -1;
-    }
-
-    return 1;
 }
 
-static int32_t spi_single_command(uint8_t cmd, uint8_t transfer_size) {
+static int32_t spi_single_command(uint8_t cmd, uint8_t transfer_size, ti_errc_t* errc) {
     if (transfer_size < 1 || transfer_size > 4) {
+        *errc = TI_ERRC_INVALID_ARG;
+
         return -1;
     }
 
     uint8_t src[4] = {cmd, 0, 0, 0};
     uint8_t dst[4] = {0, 0, 0, 0};
 
-    spi_transfer_sync(DEV.inst, DEV.ss_pin, src, dst, transfer_size);
+    *errc = spi_transfer_sync(DEV.inst, DEV.ss_pin, src, dst, transfer_size);  
+
+    if (*errc != TI_ERRC_NONE) {
+        return -1;
+    }
 
     if (transfer_size == 1) {
         return dst[0];
@@ -100,7 +128,7 @@ void adc_init(struct adc_spi_dev *dev, ti_errc_t* errc) {
     DEV  = *dev;
 
     // Reset ADC
-    uint8_t err = spi_single_command(RESET, 1);
+    uint8_t err = spi_single_command(RESET, 1, errc);
     if (err == -1 || *errc != TI_ERRC_NONE) {
         return;
     }
@@ -114,7 +142,7 @@ void adc_init(struct adc_spi_dev *dev, ti_errc_t* errc) {
     while (!is_ready) {
         uint8_t status_reg = spi_rreg(STATUS_REG, 1, errc);
         
-        if ((status_reg & 0x40) == 0 && *errc == TI_ERRC_NONE) {
+        if ((status_reg & RDY_FLAG) == 0 && *errc == TI_ERRC_NONE) {
             is_ready = true;
         } else if (timeout == 0) {
             *errc = TI_ERRC_TIMEOUT;
@@ -125,7 +153,7 @@ void adc_init(struct adc_spi_dev *dev, ti_errc_t* errc) {
     }
 
     // Enable internal reference 
-    spi_wreg(0x05, 1, 0x12, errc);
+    spi_wreg(REF_REG, 1, 0x12, errc);
 }
 
 int adc_read_voltage(const struct adc_channel* channel, ti_errc_t* errc) {
@@ -138,15 +166,15 @@ int adc_read_voltage(const struct adc_channel* channel, ti_errc_t* errc) {
 
     // Configure Input Multiplexer
     uint8_t mux_val = (channel->pos_pin << 4) | (channel->neg_pin & 0x0F);
-    spi_wreg(0x02, 1, mux_val, errc); // Returns -1 if unexpected return, otherwise 1
+    spi_wreg(INPMUX_REG, 1, mux_val, errc); // Returns -1 if unexpected return, otherwise 1
 
     // Set gain
     uint8_t pga_val = 0x08 | (channel->gain & 0x07);
-    spi_wreg(0x03, 1, pga_val, errc);
+    spi_wreg(PGA_REG, 1, pga_val, errc);
 
     // Set reference voltage
     uint8_t ref_val = 0x12 | ((channel->source & 0x03) << 2);
-    spi_wreg(0x05, 1, ref_val, errc);
+    spi_wreg(REF_REG, 1, ref_val, errc);
 
     if (*errc != TI_ERRC_NONE) {
         return -1;
@@ -154,12 +182,12 @@ int adc_read_voltage(const struct adc_channel* channel, ti_errc_t* errc) {
 
     // Wait for device ready flag
     int timeout = 100;
-    while ((spi_rreg(STATUS_REG, 1, errc) & 0x40) != 0 && timeout > 0) {
+    while ((spi_rreg(STATUS_REG, 1, errc) & RDY_FLAG) != 0 && timeout > 0) {
         timeout--;
     }
 
     // Request data
-    int32_t result = spi_single_command(RDATA, 4);
+    int32_t result = spi_single_command(RDATA, 4, errc);
 
     // If result is negative, ensure that top eight bit are flipped to 1
     if (result & 0x800000) {
@@ -192,14 +220,14 @@ void adc_set_idac(enum idac_mag magnitude, enum adc_pin pin1, enum adc_pin pin2,
     }
 
     // Set IDAC magnitude
-    spi_wreg(0x06, 1, magnitude, errc);
+    spi_wreg(IDACMAG_REG, 1, magnitude, errc);
 
     if (*errc != TI_ERRC_NONE) {
         return;
     }
 
     uint8_t mux_pins = ((pin2 & 0x0F) << 4) | (pin1 & 0x0F);
-    spi_wreg(0x07, 1, mux_pins, errc);
+    spi_wreg(IDACMUX_REG, 1, mux_pins, errc);
 }
 
 void adc_set_gpio(enum adc_pin pin, bool default_high, bool input, ti_errc_t* errc) {
@@ -214,7 +242,7 @@ void adc_set_gpio(enum adc_pin pin, bool default_high, bool input, ti_errc_t* er
     // Enable GPIO function
     uint8_t gpiocon_val;
     gpiocon_val |= (1 << gpio_idx);
-    spi_wreg(0x11, 1, gpiocon_val, errc);
+    spi_wreg(GPIOCON_REG, 1, gpiocon_val, errc);
     if (*errc != TI_ERRC_NONE) {
         return;
     }
@@ -229,7 +257,7 @@ void adc_set_gpio(enum adc_pin pin, bool default_high, bool input, ti_errc_t* er
     } 
 
     // Set as output/input and default high/low
-    spi_wreg(0x10, 1, gpiodat_val, errc);
+    spi_wreg(GPIODAT_REG, 1, gpiodat_val, errc);
 }
 
 char* adc_get_channel_name(struct adc_channel channel) {
@@ -244,6 +272,5 @@ char* adc_get_channel_name(struct adc_channel channel) {
  * 
  * 2. TODO: Reduce magic numbers. For example STATUS_REG & 0x40 is RDY_FLAG.
  * 
- * 3. TODO: Double check voltage math. Would prob be good to have someone else look through it and make sure 
- * it tracks the documentation. Are floats ok? 
+ * 4. If errc is not TI_ERRC_NONE the return value has no meaning **
  */
